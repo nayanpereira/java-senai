2eb7227a11d595d6f71026d7958377357d522def
ALUNO <nayan.estudos@gmail.com>
2026-09-11 10:57:24 -0300

    questão 22

--- Atividades/Algoritmo1.java
+++ Atividades/Algoritmo1.java
@@ -1,4 +1,4 @@
-public class App {
+public class Algoritmo1 {
     // Aula git e github
     public static void main(String[] args) throws
 Exception {
--- Atividades/Algoritmo22.java
+++ Atividades/Algoritmo22.java
@@ -5,16 +5,61 @@ cartão e pedir para ele procurar o gerente caso contrário abra uma janela perg
 O empréstimo máximo é 20.000. Pergunte a quantidade de parcelas e mostre o valor das parcelas com juros de 1% ao mês. As parcelas podem ser 
 divididas em até 10 meses. Mostre o valor total do financiamento, o valor financiado e a mensagem "Parabéns, seja bem vindo a família SENATECH.*/
 
+// usuário senha - processamento fazer login se errar três vezes bloquear o cartão e mandar procurar o gerente, se passar oferecer emprestimo, perguntando quanto deseja simular,
+// máximo do banco 20 mil, perguntar a quantidade de parcelas e mostre o valor das parcelar com juros de 1% ao mês. Até 10 parcelas , 10 meses para pagar, 
+// no final mostrar o valor toral financiado e a mensagem parabéns  "Parabéns, seja bem vindo a família SENATECH
+
 public class Algoritmo22 {
     public void main() {
+        //Entrada
+
         // Atributos
         String usuario = "senai";
         String usuarioDigitado;
         int senha = 123;
         int senhaDigitada;
+        int contador = 1;
 
-        senhaDigitada = Integer.parseInt(IO.readln("Digite a senha: "));
 
+
+        while(contador < 4){
+            usuarioDigitado = (IO.readln("Digite o usuário: "));
+            senhaDigitada = Integer.parseInt(IO.readln("Digite a senha: "));
+
+            if (!usuario.equals(usuarioDigitado)){
+                IO.println("Usuário incorreto: contador");
+                contador += 1;
+            }
+            else if (senha != senhaDigitada) {
+                IO.println("Senha incorreto: contador");
+                contador += 1;
+            }
+            else {
+                IO.println("Login realizado com sucesso: ");
+                break;
+
+            }
+        }
+
+        if (contador == 3) {
+            IO.println("Login bloquado, procure o gerente: ");
+
+        }else {
+            IO.println("\n------------Emprestimo------------");
+            //int valor = Integer.parseInt(IO.readln("quanto vai precisar de emprestimo? "));
+            float valor = Float.parseFloat(IO.readln("Quanto vai precisar de emprestimo? "));
+            int parcelas = Integer.parseInt(IO.readln("Pretende pagar em quantas parcelas? Limite de 10 parcelas: "));
+
+            IO.println("\n------Condições de pagamento------");
+            IO.println("Emprestido de R$: " + valor + " em " + parcelas + " parcelas");
+
+            IO.println("\n----------------------------------");
+            IO.println("Parabéns, seja bem vindo a família SENATECH ");            
+            IO.println("Emprestimo de R$ " + valor + " Realizado" );
+            IO.println("Em "+ parcelas + " parcelas de R$ " + (valor/parcelas));
+
+
+        }
 
 
 
